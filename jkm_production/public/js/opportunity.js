5a08d16402609268bbe48061289e2697a77c328b
viral <viral@fosserp.com>
2024-06-20 05:47:58 +0000

    changes

--- jkm_production/public/js/opportunity.js
+++ jkm_production/public/js/opportunity.js
@@ -16,15 +16,12 @@ frappe.ui.form.on("Opportunity", {
 		if(frm.doc.opportunity_from == "Lead"){
 			console.log('get')
 			frappe.model.get_value("Lead", frm.doc.party_name, ['custom_mobile_number', 'email_id', 'custom_designation'],  r=>{
-                if(!frm.doc.contact_email){
                     frm.set_value('contact_email', r.email_id)
-                }
-                if(!frm.doc.contact_mobile){
+            
                     frm.set_value('contact_mobile', r.custom_mobile_number)
-                }
-                if(!frm.doc.job_title){
+                
                     frm.set_value('job_title', r.custom_designation)
-                }
+            
 			})
 		}
 	}
