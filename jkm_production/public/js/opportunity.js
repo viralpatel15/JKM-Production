556f6d112cf64a65ac3eaede1ba4ee78ec692482
viral <viral@fosserp.com>
2024-06-20 05:39:22 +0000

    changes

--- jkm_production/public/js/opportunity.js
+++ jkm_production/public/js/opportunity.js
@@ -4,7 +4,6 @@ frappe.ui.form.on("Opportunity", {
     refresh:(frm, cdt, cdn)=>{
         frm.fields_dict['items'].grid.update_docfield_property('rate', 'reqd', 1);		
         frm.fields_dict['items'].grid.update_docfield_property('amount', 'reqd', 1);
-        frm.save()	
     },
     item_code:(frm, cdt, cdn)=>{
         frm.fields_dict['items'].grid.update_docfield_property('rate', 'reqd', 1);		
