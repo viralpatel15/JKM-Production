b192bb047a9304f14148bca5c083ceaf259d0d23
viralkansodiya <viral@fosserp.com>
2024-07-04 16:11:50 +0000

    ch

--- jkm_production/jkm_production/doctype/courier_agency/courier_agency.js
+++ jkm_production/jkm_production/doctype/courier_agency/courier_agency.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Courier Agency', {
-	// refresh: function(frm) {
-
-	// }
+	address: function(frm) {
+		frappe.call({
+			method: "frappe.contacts.doctype.address.address.get_address_display",
+			args: { address_dict: frm.doc['address'] },
+			callback: function (r) {
+				if (r.message) {
+					frm.set_value('address_display', r.message);
+				}
+			},
+		});
+	},
+	contact_details: function(frm) {
+		frappe.call({
+			method: "frappe.contacts.doctype.contact.contact.get_contact_details",
+			args: { contact: frm.doc.contact_details },
+			callback: function (r) {
+				if (r.message) frm.set_value(r.message);
+			},
+		});
+	}
 });
