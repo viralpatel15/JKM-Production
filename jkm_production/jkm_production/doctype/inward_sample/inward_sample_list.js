ec08b94352a5ff0288954dacd29dfeec3bdc50db
viral <viral@fosserp.com>
2024-07-09 11:09:33 +0000

    ch

--- jkm_production/jkm_production/doctype/inward_sample/inward_sample_list.js
+++ jkm_production/jkm_production/doctype/inward_sample/inward_sample_list.js
@@ -7,16 +7,13 @@ frappe.listview_settings["Inward Sample"] = {
 		"supplier_name"
 	],
 	get_indicator: function (doc) {
-		if(doc.status == "Draft"){
-			return [__(doc.status), "red", "status,=," + doc.status];
-		}else if(doc.status == "Requested"){
-			return [__(doc.status), "blue", "status,=," + doc.status];
-		}else if(doc.status == "Ordered"){
-			return [__(doc.status), "green", "status,=," + doc.status];
-		}else if(doc.status == "Dispatched"){
-			return [__(doc.status), "orange", "status,=," + doc.status];
-		}else if(doc.status == "Delivered"){
-			return [__(doc.status), "green", "status,=," + doc.status];
-		}
+		const status_colors = {
+			'Draft': "grey",
+			'Requested': "orange",
+			'Ordered': "green",
+			'Dispatched': "gray",
+			'Delivered': "red",
+		};
+		return [__(doc.status), status_colors[doc.status], "status,=," + doc.status];
 	},
 };
