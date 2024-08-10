9ce5fe64122a242c6966fe4b26bfb80bd37fcaf7
viral <viral@fosserp.com>
2024-08-10 05:10:11 +0000

    ch

--- jkm_production/public/js/supplier_quotation.js
+++ jkm_production/public/js/supplier_quotation.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on("Supplier Quotation", {
     refresh:(frm)=>{
+
         frm.doc.items.forEach(e => {
             if(!e.custom_exchange_rate){
                 frappe.model.set_value(e.doctype, e.name, "custom_exchange_rate", 1)
@@ -9,9 +10,42 @@ frappe.ui.form.on("Supplier Quotation", {
             }
         });
         frm.refresh_field("items")
+    },
+    custom_transporter:(frm)=>{
+        frappe.model.get_value("Supplier", frm.doc.custom_transporter, "supplier_name", r=>{
+            frm.set_value("custom_transporter_name", r.supplier_name)
+        })
+    },
+    custom_transporter_changers_:(frm)=>{
+        calculate_transporter_charges(frm)
+    },
+    custom_unloading_at_destination_:(frm)=>{
+        calculate_transporter_charges(frm)
+    },
+    custom_local_transport_charges_at_designation:(frm)=>{
+        calculate_transporter_charges(frm)
+    },
+    custom_local_transport_charges_at_origin:(frm)=>{
+        calculate_transporter_charges(frm)
+    },
+    custom_loading_charges_at_origin:(frm)=>{
+        calculate_transporter_charges(frm)
+    },
+    custom_other_charges:(frm)=>{
+        calculate_transporter_charges(frm)
     }
+
+
 })
 
+cur_frm.fields_dict["custom_transporter"].get_query = function (doc) {
+	// filter on Account
+	return {
+		filters: {
+			is_transporter : 1
+		},
+	};
+};
 frappe.ui.form.on("Supplier Quotation Item", {
     custom_rate_currency:(frm, cdt, cdn) => {
         let d = locals[cdt][cdn]
@@ -91,4 +125,9 @@ function calculate_cbm(frm, cdt, cdn){
         })
         frm.set_value("custom_total_cbm", total_cbm)
     }
+}
+
+function calculate_transporter_charges(frm){
+    let ltch = flt(frm.doc.custom_local_transport_charges_at_origin)+flt(frm.doc.custom_transporter_changers_) + flt(frm.doc.custom_transporter_changers_) + flt(frm.doc.custom_local_transport_charges_at_designation) + flt(frm.doc.custom_local_transport_charges_at_designation) + flt(frm.doc.custom_loading_charges_at_origin) + flt(frm.doc.custom_other_charges) + flt(frm.doc.custom_unloading_at_destination_)
+    frm.set_value('custom_total_transportation_expenses', ltch)
 }
