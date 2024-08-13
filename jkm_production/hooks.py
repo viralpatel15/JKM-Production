089f62bdc3b7bed7b31d612da5db8f2abdb7938b
viral <viral@fosserp.com>
2024-08-13 04:45:12 +0000

    ch

--- jkm_production/hooks.py
+++ jkm_production/hooks.py
@@ -108,7 +108,8 @@ doctype_js = {
     "Journal Entry": "public/js/doctype_js/journal_entry.js",
     "Sales Order" : "public/js/doctype_js/sales_order.js",
     "Delivery Note":"public/js/doctype_js/delivery_note.js",
-    "Purchase Invoice":"public/js/doctype_js/purchase_invoice.js"
+    "Purchase Invoice":"public/js/doctype_js/purchase_invoice.js",
+    "Request for Quotation":"public/js/request_for_quotation.js"
 }
 
 
