8ca095d0ce5607f9c72ea2509cd7739f7b7e2612
viral <viral@fosserp.com>
2024-08-23 12:14:58 +0000

    ch

--- jkm_production/jkm_production/doc_events/request_for_quotation.py
+++ jkm_production/jkm_production/doc_events/request_for_quotation.py
@@ -4,3 +4,23 @@ def on_submit(self,method):
     if self.workflow_state == "In Progress":
         if not len(self.suppliers):
             frappe.throw("Supplier Details is requiered to set RFQ")
+
+            
+from erpnext.buying.doctype.request_for_quotation.request_for_quotation import RequestforQuotation
+
+class jkmrequestforquotation(RequestforQuotation):
+    def send_to_supplier(self):
+        """Sends RFQ mail to involved suppliers."""
+        for rfq_supplier in self.suppliers:
+            if rfq_supplier.email_id is not None and rfq_supplier.send_email:
+                self.validate_email_id(rfq_supplier)
+
+                # make new user if required
+                update_password_link, contact = self.update_supplier_contact(rfq_supplier, self.get_link())
+
+                self.update_supplier_part_no(rfq_supplier.supplier)
+                self.supplier_rfq_mail(rfq_supplier, update_password_link, self.get_link())
+                rfq_supplier.email_sent = 1
+                if not rfq_supplier.contact:
+                    rfq_supplier.contact = contact
+                rfq_supplier.save()
