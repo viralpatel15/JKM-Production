052774627fe06a1a8c10daf4deb16100b36b8877
root <root@jkmchemtrade.com>
2024-12-18 04:28:52 +0000

    ch

--- jkm_production/jkm_production/doc_events/sales_order.py
+++ jkm_production/jkm_production/doc_events/sales_order.py
@@ -32,9 +32,7 @@ def validate(self, method):
 	self.custom_gross_weight = custom_gross_weight
 	self.custom_total_cbm = custom_total_cbm
 	self.custom_total_packages = custom_total_packages
-	if self.doctype == "Sales Order":
-		self.custom_qr = generate_upi_qr_code(upi_id = "mavanidhaval24@okhdfcbank", payee_name = self.customer, amount=self.grand_total)
-
+	
 
 @frappe.whitelist()
 def make_sales_order(source_name: str, target_doc=None):
@@ -66,7 +64,6 @@ def generate_upi_url(upi_id, payee_name, amount):
 		"am": f"{amount:.2f}",  # Amount (2 decimal places)
 		"cu": "INR"  # Currency
 	}
-	frappe.throw(str(f"{base_url}?{urllib.parse.urlencode(query_params)}"))
 	return f"{base_url}?{urllib.parse.urlencode(query_params)}"
 
 
@@ -85,7 +82,6 @@ def generate_upi_qr_code(upi_id = "mavanidhaval24@okhdfcbank", payee_name = "sup
 	
 	# Convert QR code to an image
 	img = qr.make_image(fill="black", back_color="white")
-	frappe.throw(str(img))
 	# Save to a BytesIO stream
 	buffer = BytesIO()
 	img.save(buffer, format="PNG")
